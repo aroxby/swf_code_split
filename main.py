5575b9b860f81e290b6c1cd5d85f822fd32a5f53
Andy Roxby <Andy.Roxby@gmail.com>
2016-04-06 00:17:33 -0400

    Layout processing structure, nothing exciting yet

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import codecs
+
+def raw_bytes_to_str(bytes):
+    return ''.join(map(chr, bytes))
+
+PKG_MARKER = raw_bytes_to_str(codecs.BOM_UTF8)
+
+def usage():
+    print('Usage: swf_code_split.py FILE');
+
+def find_class(file):
+    CLASS_MARKER_REGEX_STR = '\tpublic (class)|(namespace) (?P=class)'
+    for line in file:
+        if line.startswith(CLASS_MARKER):
+            return line[len(CLASS_MARKER):]
+    return None
+    
+def find_package(file):
+    PKG_MARKER_REGEX_STR = 'package (?P=package)'
+    return rx.match(PKG_MARKER_REGEX_STR)['package']
+
+def process_file(file):
+    print(file.split('\n')[0])
+
+def code_split(path):
+    with open(path) as fp:
+        contents = fp.read()
+        packed_files = contents.split(PKG_MARKER)
+        for item in packed_files:
+            if len(item.strip())!=0:
+                process_file(item)
+    return 0
+
+def main():
+    if len(sys.argv) != 2:
+        usage();
+        sys.exit(1)
+    else:
+        sys.exit(code_split(sys.argv[1]))
+
+if __name__ == '__main__':
+    main()
